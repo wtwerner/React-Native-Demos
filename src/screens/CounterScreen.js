975b482d7b93ea79d50f07d0b6b739d158a0d339
wtwerner <wtwerner@gmail.com>
2021-04-26 16:32:40 -0500

    Create counter screen

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+const CounterScreen = () => {
+  return (
+    <View>
+      <Text>Current Count</Text>
+    </View>
+  );
+};
+
+export default CounterScreen;
+
+const styles = StyleSheet.create({});
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -17,6 +17,10 @@ const HomeScreen = ({ navigation }) => {
         title="Go to Image Demo"
         onPress={() => navigation.navigate("Image")}
       />
+      <Button
+        title="Go to Counter"
+        onPress={() => navigation.navigate("Counter")}
+      />
     </View>
   );
 };
