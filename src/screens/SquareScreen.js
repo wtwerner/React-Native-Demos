59b567e2f9daa55e668f6cd225a36cad51b586ac
wtwerner <wtwerner@gmail.com>
2021-04-27 14:18:49 -0500

    Add square demo screen

---
+++ src/screens/SquareScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const SquareScreen = () => {
+    return (
+        <View>
+            <Text>Square Screen</Text>
+        </View>
+    );
+};
+
+export default SquareScreen;
+
+const styles = StyleSheet.create({});
