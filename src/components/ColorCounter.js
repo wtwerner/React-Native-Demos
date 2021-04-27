959935682ba6143315d95a590c0dd034c7074479
wtwerner <wtwerner@gmail.com>
2021-04-27 14:26:41 -0500

    Create color counter component

---
+++ src/components/ColorCounter.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+const ColorCounter = ({ color }) => {
+    return (
+        <View>
+            <Text>{color}</Text>
+            <Button title={`Increase ${color}`} />
+            <Button title={`Decrease ${color}`} />
+        </View>
+    );
+};
+
+export default ColorCounter;
+
+const styles = StyleSheet.create({});
