ff8fb134894c05353e21fa5c59e3abcc361010c7
wtwerner <wtwerner@gmail.com>
2021-04-27 20:52:54 -0500

    Add box screen component

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const BoxScreen = () => {
+    return (
+        <View>
+            <Text>Box screen</Text>
+        </View>
+    );
+};
+
+export default BoxScreen;
+
+const styles = StyleSheet.create({});
