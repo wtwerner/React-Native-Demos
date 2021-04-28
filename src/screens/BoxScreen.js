cee2d82db685fc15aca8301814bd0c0500d7f442
wtwerner <wtwerner@gmail.com>
2021-04-28 10:52:43 -0500

    Play with box positioning

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,12 +3,23 @@ import { StyleSheet, Text, View } from 'react-native';
 
 const BoxScreen = () => {
     return (
-        <View>
-            <Text>Box screen</Text>
+        <View style={styles.viewStyle}>
+            <Text style={styles.textStyle}>child 1</Text>
+            <Text style={styles.textStyle}>child 2</Text>
+            <Text style={styles.textStyle}>child 3</Text>
         </View>
     );
 };
 
 export default BoxScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 3,
+        borderColor: 'black',
+    },
+    textStyle: {
+        borderWidth: 3,
+        borderColor: 'red',
+    },
+});
